98f73fbc47410d858aaebcff20f481c15df4143e
CarlosLV555 <carlosiglo99@gmail.com>
2024-12-09 15:27:45 +0100

    Add files via upload

---
+++ data/Fahrradparkpltze_7.js
@@ -0,0 +1,1 @@
+var json_Fahrradparkpltze_7 = {"type":"FeatureCollection","name":"Fahrradparkpltze_7","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Dach.Art":"Offen"},"geometry":{"type":"Point","coordinates":[7.0316175,49.3439904]}}]}
---
+++ data/Shuttle_2.js
@@ -0,0 +1,1 @@
+var json_Shuttle_2 = {"type":"FeatureCollection","name":"Shuttle_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"Point","coordinates":[7.03266003909652,49.34336361012825]}}]}
